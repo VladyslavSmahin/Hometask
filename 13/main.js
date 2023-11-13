8a727d213407cf02a7aff47ce3ac48fa376bc63c
Uzumaki.info <vladislavsmagin1@gmail.com>
2023-11-13 19:03:00 +0200

    11

--- 13/main.js
+++ 13/main.js
@@ -1,6 +1,8 @@
 function slider(prevButton, nextButton, listElement) {
+    const ACTIVE_CLASS_NAME = `current`
+    let slides = document.querySelector(`ul`).children
+    console.log(slides)
     function changeElement (event)  {
-        const ACTIVE_CLASS_NAME = `current`
         const isPrevButton = event.target === prevButton
         const activeElement = listElement.querySelector(`.${ACTIVE_CLASS_NAME}`)
         const prevOrNextElement = isPrevButton
@@ -11,8 +13,7 @@ function slider(prevButton, nextButton, listElement) {
             activeElement.classList.remove(ACTIVE_CLASS_NAME)
             prevOrNextElement.classList.add(ACTIVE_CLASS_NAME)
         }
-        let slides = document.querySelector(`ul`).children
-        console.log(slides)
+
         function removeButton (button, slide) {
             if (slide.classList.contains(`current`)) {
                 button.style.display =`none`
@@ -25,6 +26,20 @@ function slider(prevButton, nextButton, listElement) {
     }
     prevButton.addEventListener(`click`, changeElement)
     nextButton.addEventListener(`click`, changeElement)
+
+    let dots1 = document.querySelector(`.dots1`)
+    console.log(dots1)
+    let dots2 = document.querySelector(`.dots2`)
+    let dots3 = document.querySelector(`.dots3`)
+    let dots4 = document.querySelector(`.dots4`)
+    let dots5 = document.querySelector(`.dots5`)
+    function dotsSlides (dot, slideOn) {
+        dot.addEventListener(`click`, function (){
+            slides.forEach(s => s.classList.remove(ACTIVE_CLASS_NAME));
+            slideOn.classList.add(ACTIVE_CLASS_NAME)
+        })
+    }
+    dotsSlides(dots1,slides[0])
 }
 slider(
     document.querySelector(`.js--slider__prev`),

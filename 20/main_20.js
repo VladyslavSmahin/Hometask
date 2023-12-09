3cde5c7107292155835e538525a0525e7bbb92f1
Uzumaki.info <vladislavsmagin1@gmail.com>
2023-12-09 16:10:49 +0200

    20

--- 20/main_20.js
+++ 20/main_20.js
@@ -1,30 +1,48 @@
-const body = $(`body`)
+const img = $(`.js-onClick-img`)
 
 function modalWindow(event) {
     function changeContent() {
-        const images = ['./img/1.jpg', './img/2.jpg', './img/3.jpg', './img/4.jpg', './img/5.jpg', './img/6.jpg'];
-        let currentImageIndex = 0;
+        let $savedElement
 
         function currentIMG() {
             $('.js--gal_item').on('click', function () {
+                $savedElement = $(this)
                 const imgSrc = $(this).find('img').attr('src');
-
                 $('.js--modal__content img').attr('src', imgSrc);
             });
-
         }
 
         function nextIMG() {
             $('.js--modal__next').on('click', function () {
-                currentImageIndex = (currentImageIndex + 1) % images.length; //
-                $('.js--modal__content img').attr('src', images[currentImageIndex]);
+                if ($savedElement) {
+                    const newElement = $savedElement.closest('.js--gal_item').next().find('img');
+                    if (newElement.length > 0) {
+                        const newImgSrc = newElement.attr('src');
+                        $('.js--modal__content img').attr('src', newImgSrc);
+                        $savedElement = newElement.closest('.js--gal_item')
+                    } else {
+                        const firstImgSrc = $('.js--gal_item:first-of-type img').attr('src');
+                        $('.js--modal__content img').attr('src', firstImgSrc);
+                        $savedElement = $('.js--gal_item:first-of-type');
+                    }
+                }
             });
         }
 
         function prevIMG() {
             $('.js--modal__prev').on('click', function () {
-                currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-                $('.js--modal__content img').attr('src', images[currentImageIndex]);
+                if ($savedElement) {
+                    const newElement = $savedElement.closest('.js--gal_item').prev().find('img');
+                    if (newElement.length > 0) {
+                        const newImgSrc = newElement.attr('src');
+                        $('.js--modal__content img').attr('src', newImgSrc);
+                        $savedElement = newElement.closest('.js--gal_item')
+                    } else {
+                        const lastImgSrc = $('.js--gal_item:last-of-type img').attr('src');
+                        $('.js--modal__content img').attr('src', lastImgSrc);
+                        $savedElement = $('.js--gal_item:last-of-type');
+                    }
+                }
             });
         }
 
@@ -33,17 +51,20 @@ function modalWindow(event) {
         currentIMG()
     }
 
-    function openCloseModal() {
+    function openModal() {
         const target = event.target;
         if ($(target).hasClass(`js--gal_item`) || $(target).hasClass(`js-onClick-img`)) {
             changeContent()
             $(`.modal`).css(`display`, `block`)
-        } else if ($(target).hasClass(`js--modal__close`)) {
-            $(`.modal`).css(`display`, `none`)
         }
     }
 
-    openCloseModal()
+    function closeModal() {
+        $(`.js--modal__close`).on(`click`, () => $(`.modal`).css(`display`, `none`))
+    }
+
+    closeModal()
+    openModal()
 }
 
-body.on(`click`, modalWindow)
+img.on(`click`, modalWindow)

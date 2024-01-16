ba91190e3937045ca64780c0ef8166ff7c5b4ae1
Uzumaki.info <vladislavsmagin1@gmail.com>
2024-01-16 19:33:30 +0200

    26+27

--- 27/my-app/src/Components/counter.jsx
+++ 27/my-app/src/Components/counter.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Counter (props){
-    const {id, children} = props
+function Counter (props) {
+    const {id, value} = props
 
-return  <h2 id={id}>{children}  </h2>
+    return <h2 id={id}>  {value}</h2>;
 }
 export default Counter
--- 27/my-app/src/main.jsx
+++ 27/my-app/src/main.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Button from "./Components/Button.jsx";
 import Smile from "./Components/smile.jsx";
 import Counter from "./Components/counter.jsx";
-import {useState, useEffect} from "react";
 
 export function Main() {
+    const initialState = [
+        { id: 'count1', value: 0 },
+        { id: 'count2', value: 0 },
+        { id: 'count3', value: 0 },
+        { id: 'count4', value: 0 },
+        { id: 'count5', value: 0 },
+    ];
     const [showSpan, setShowSpan] = useState(false);
-    const [maxCounter, setMaxCounter] = useState(0);
-    const [showSmile, setShowSmile] = useState(null);
-    const [counters, setCounters] = useState([
-        { id: 'count1', value: 0},
-        { id: 'count2', value: 0},
-        { id: 'count3', value: 0},
-        { id: 'count4', value: 0},
-        { id: 'count5', value: 0},
-    ]);
+    const [counters, setCounters] = useState(initialState);
+    const [winner, setWinner] = useState( { id: 'count1', value: 0 });
 
         const [smilesAndCounters, setSmilesAndCounters] = useState([
             {id: '1', smileSrc: '/smiles/1.svg', counterKey: 'count1'},
@@ -27,54 +26,47 @@ export function Main() {
             {id: '5', smileSrc: '/smiles/5.svg', counterKey: 'count5'},
         ],)
 
-
+    useEffect(() => {
+        localStorage.setItem('counters', JSON.stringify(counters));
+        console.log('данные перезаписаны')
+    }, [counters]);
 
     useEffect(() => {
-        const storedCounters = JSON.parse(localStorage.getItem('counters'));
-        if (storedCounters) {
-            setCounters(storedCounters);
-        }
-    }, []);
+        JSON.parse(localStorage.getItem('counters'));
 
+    }, []);
 
     const handleSmileClick = (id) => {
         setCounters((prevCounters) => {
             return prevCounters.map((counter) => {
                 if (counter.id === id) {
-                    return { ...counter, value: counter.value + 1 };
+                    return {...counter, value: counter.value + 1};
                 }
                 return counter;
             });
         });
     };
 
-    const handleButtonClick = () => {
-        /*const values = Object.values(counters);
-        const maxCounter = Math.max(...values)
-        const maxCounterKey = Object.keys(counters).find(
-            key => counters[key] === maxCounter
-        );
-        const showSmile = smilesAndCounters.find(item => item.counterKey === maxCounterKey)?.smileSrc;
-        setShowSpan(true);
-        setMaxCounter(maxCounter);
-        setShowSmile(showSmile);*/
 
-    }
+    const handleButtonClick = () => {
+        const maxCounterObject = counters.reduce((maxCounterObject, counter) => {
+            return counter.value > maxCounterObject.value ? counter : maxCounterObject;
+        }, { id: '', value: 0 });
+        setShowSpan(true);
+        const winningSmile = smilesAndCounters.find(item => item.counterKey === maxCounterObject.id);
+        if (winningSmile) {
+            setWinner({ id: winningSmile.smileSrc, value: maxCounterObject.value });
+        }
+    };
     const clearAll = () => {
         localStorage.clear()
-        setShowSmile( null)
-        /*setCounters({
-            count1: 0,
-            count2: 0,
-            count3: 0,
-            count4: 0,
-            count5: 0
-        })*/
+        setShowSpan(false)
+        setCounters([...initialState])
     }
     const size = {width: "100px", height: "100px"};
     const flexCentre = "d-flex justify-content-center align-items-center";
     const divStyle = {
-        visibility: showSmile ? 'visible' : 'hidden'
+        visibility: winner ? 'visible' : 'hidden'
     };
     return (
         <React.StrictMode>
@@ -88,9 +80,11 @@ export function Main() {
                             clName={'m-4'}
                             onClick={handleSmileClick}
                             id={`count${item.id}`}/>
-                        <Counter id={`count${item.id}`}>
-                            {counters[`count${item.id}`]}
-                        </Counter>
+                        <Counter
+                            id={`count${item.id}`}
+                            value={counters.find(counter => counter.id === `count${item.id}`).value} />
+
+
                     </div>
                 ))}
             </div>
@@ -99,8 +93,8 @@ export function Main() {
                 {showSpan && (
                     <>
                         <div className={'result'} style={divStyle}>
-                            <h3 className={'m-4'}> Winner: {maxCounter}</h3>
-                            <Smile src={showSmile} alt={'img'} size={size}/>
+                            <h3 className={'m-4'}> Winner: {winner.value}</h3>
+                            <Smile src={winner.id} alt={'img'} size={size}/>
                         </div>
                         <Button pos="center" text="Clear Results" onClick={clearAll}/>
                     </>

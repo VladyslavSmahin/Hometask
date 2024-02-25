078a89ea00d597af2ee554f1dcb9797d5caf5436
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-02-25 18:40:27 +0200

    дипломная 25.02

--- 31/counter-react-redux/src/ui/components/todo/Footer.jsx
+++ 31/counter-react-redux/src/ui/components/todo/Footer.jsx
@@ -2,10 +2,11 @@ import Button from "../todo/Button.jsx";
 import selectors from "../../../engine/todo/redux/selectors.js";
 import {useSelector, useDispatch} from "react-redux";
 import {clearStoreAsyncAction, filterItemsAsyncAction} from "../../../engine/todo/saga/asynxActions.js";
-import {Box, Typography} from "@mui/material";
+import {Box, TextField, Typography} from "@mui/material";
 import Input from "./Input.jsx";
 import todoSlice from "../../../engine/todo/redux/todoSlice.js";
 import {useEffect} from "react";
+import {Form} from "formik";
 
 function Footer() {
     const items = useSelector(selectors.items);
@@ -17,11 +18,16 @@ function Footer() {
         const inputValue = event.target.value.toLowerCase();
         dispatch(filterItemsAsyncAction(inputValue));
     }
+    const onReset = (event) => {
+        console.log(event.target.value)
+    }
     return (
         <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-            <Typography sx={{ border: '1px solid black', padding:"10px"}} variant="h5">Count: {items.length}</Typography>
-            <Button onClick={onClear} sx={{marginRight:'6px'}}>Clear</Button>
-            <Input sx={{width:"30%"}} onChange={handleFilter}></Input>
+            <Typography sx={{border: '1px solid black', padding: "10px"}}
+                        variant="h5">Count: {items.length}</Typography>
+            <Button onClick={onClear} sx={{marginRight: '6px'}}>Clear</Button>
+            <Input  sx={{width: "50%"}} onChange={handleFilter}></Input>
+
         </Box>
     );
 }
